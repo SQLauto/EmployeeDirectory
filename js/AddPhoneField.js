4e45e4d86c802c3d153add1e2f4864681914f2b3
Rich Burns <rich.burns81@gmail.com>
2014-10-05 14:09:57 -0700

    Reworked JS and form
    Messed around with the form and field cloning JS.  It works, but it
    could use some refinement

--- js/AddPhoneField.js
+++ js/AddPhoneField.js
@@ -1,17 +1,29 @@
-var PhoneNumAdd =
-{
-    init: function () {
-        var link = document.getElementById("addPhoneNum");
-        link.addEventListener("click", PhoneNumAdd.addField, false)
-    },
-    addField: function () {
-        var list = document.getElementById("phoneList");
-        var listItem = list.getElementsByTagName("div");
-        var addButton = document.getElementById("addPhoneNum");
-        var clone = listItem[0].cloneNode(true);
-        list.insertBefore(clone, addButton);
-        console.log(list);
-        console.log(listItem);
+document.getElementById("addPhoneNum").onclick = addField;
+document.getElementById("submit").onclick = fieldIncrementer;
+   
+function addField() {
+    var list = document.getElementById("phoneList");
+    var listItem = list.getElementsByTagName("div");
+    var clone = listItem[0].cloneNode(true);
+    list.appendChild(clone);
+}
+
+function fieldIncrementer(){
+    var list = document.getElementById("phoneList");
+    var divs = list.getElementsByClassName("form-group");
+    var labels = list.getElementsByTagName("label");
+    var inputs = list.getElementsByTagName("input");
+    var selects = list.getElementsByTagName("select");
+    for (var i = 0; i < divs.length; i++ ){
+        divs[i].attributes.id.value += i;
+        labels[i].attributes.for.value += i;
+        inputs[i].attributes.name.value += i;
+        inputs[i].attributes.id.value += i;
+        selects[i].attributes.name.value += i;
+        console.log(divs[i]);
+        console.log(labels[i]);
+        console.log(inputs[i]);
+        console.log(selects[i]);
+        document.forms[0].submit();
     }
 }
-PhoneNumAdd.init();
